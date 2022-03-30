350e64b9e858d8a8e2f37b83d7c921db8649bece
Dmitrii <drozdovdm27@gmail.com>
2022-03-30 23:34:31 +0300

    Addition to last commit

---
+++ Smev3GosuslugiBot/MessageReceiver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+
+namespace Smev3GosuslugiBot
+{
+    public class MessageReceiver : IUpdateHandler, IMessageReceiver
+    {
+        public Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeStateOf(ITelegramBotClient botClient)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IMessageReceiver
+    {
+        public void ChangeStateOf(ITelegramBotClient botClient);
+    }
+}
