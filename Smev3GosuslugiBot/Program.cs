8c0cd64d469090a12e4a1d8efe3418e2f248b4c9
Dmitrii <drozdovdm27@gmail.com>
2022-03-30 22:11:50 +0300

    Add project files.

---
+++ Smev3GosuslugiBot/Program.cs
@@ -0,0 +1,25 @@
+﻿using Telegram.Bot;
+
+namespace Smev3GosuslugiBot
+{
+    internal class Program
+    {
+        private static ITelegramBotClient _bot;
+
+        public static void Main(string[] args)
+        {
+            var token = GetToken();
+            Console.WriteLine(token);
+            Console.ReadKey();
+
+            Console.ReadKey();
+            Console.WriteLine(token);
+            //_bot = new TelegramBotClient(token);
+        }
+        
+        public static string GetToken()
+        {
+            return File.ReadAllText("token.txt");
+        }
+    }
+}
